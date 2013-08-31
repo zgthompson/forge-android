875df7d9e7b96d67ae35465176134168801cfc47
Zachary Thompson <zgthompson@gmail.com>
2013-08-31 11:16:40 -0700

    Created barebone activities

--- Forge/src/main/java/com/pockwester/forge/ClassIndexActivity.java
+++ Forge/src/main/java/com/pockwester/forge/ClassIndexActivity.java
@@ -1,30 +1,23 @@
 package com.pockwester.forge;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class ClassIndexActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_class_index);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.class_index, menu);
         return true;
-    }
-
-    public void openClassIndex(View v) {
-        Intent intent = new Intent(this, ClassIndexActivity.class);
-        startActivity(intent);
     }
     
 }
